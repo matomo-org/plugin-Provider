597b20133c351b142cb5d57acdcf719e6bdd226f
Thomas Steur <tsteur@users.noreply.github.com>
2020-02-11 16:17:34 +1300

    adding some basic test

---
+++ tests/System/APITest.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Plugins\Provider\tests\System;
+
+use Piwik\Plugins\Provider\tests\Fixtures\SimpleFixtureTrackFewVisits;
+use Piwik\Tests\Fixtures\ManyVisitsWithMockLocationProvider;
+use Piwik\Tests\Framework\TestCase\SystemTestCase;
+
+/**
+ * @group Provider
+ * @group APITest
+ * @group Plugins
+ */
+class APITest extends SystemTestCase
+{
+    /**
+     * @var SimpleFixtureTrackFewVisits
+     */
+    public static $fixture = null; // initialized below class definition
+
+    /**
+     * @dataProvider getApiForTesting
+     */
+    public function testApi($api, $params)
+    {
+        $this->runApiTests($api, $params);
+    }
+
+    public function getApiForTesting()
+    {
+        $api = array(
+            'Provider.getProvider'
+        );
+
+        $apiToTest   = array();
+        $apiToTest[] = array($api,
+            array(
+                'idSite'     => 1,
+                'date'       => self::$fixture->dateTime,
+                'periods'    => array('day'),
+                'testSuffix' => ''
+            )
+        );
+
+        return $apiToTest;
+    }
+
+    public static function getOutputPrefix()
+    {
+        return '';
+    }
+
+    public static function getPathToTestDirectory()
+    {
+        return dirname(__FILE__);
+    }
+
+}
+
+APITest::$fixture = new ManyVisitsWithMockLocationProvider();
