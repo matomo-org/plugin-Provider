96c912a3c59366f87695848d15608999503f073a
diosmosis <diosmosis@users.noreply.github.com>
2023-05-20 15:00:58 -0700

    move archiving logic to new Providers RecordBuilder

--- Archiver.php
+++ Archiver.php
@@ -9,32 +9,8 @@
 
 namespace Piwik\Plugins\Provider;
 
-use Piwik\Metrics;
-
 class Archiver extends \Piwik\Plugin\Archiver
 {
     const PROVIDER_RECORD_NAME = 'Provider_hostnameExt';
     const PROVIDER_FIELD = "location_provider";
-
-    public function aggregateDayReport()
-    {
-        $metrics = $this->getLogAggregator()->getMetricsFromVisitByDimension(self::PROVIDER_FIELD)->asDataTable();
-        $report  = $metrics->getSerialized($this->maximumRows, null, Metrics::INDEX_NB_VISITS);
-        $this->getProcessor()->insertBlobRecord(self::PROVIDER_RECORD_NAME, $report);
-    }
-
-    public function aggregateMultipleReports()
-    {
-        $columnsAggregationOperation = null;
-
-        $this->getProcessor()->aggregateDataTableRecords(
-            [self::PROVIDER_RECORD_NAME],
-            $this->maximumRows,
-            $maximumRowsInSubDataTable = null,
-            $columnToSortByBeforeTruncation = null,
-            $columnsAggregationOperation,
-            $columnsToRenameAfterAggregation = null,
-            $countRowsRecursive = []
-        );
-    }
 }
